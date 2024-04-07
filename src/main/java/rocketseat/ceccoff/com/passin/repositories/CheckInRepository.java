af3a12cc62d7e6e07eccb15b4d486ffff9e71b17
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-04-07 18:05:45 -0300

    feat: :sparkles: Adicionado funcao para JPA realizar busca de attendee por id

--- src/main/java/rocketseat/ceccoff/com/passin/repositories/CheckInRepository.java
+++ src/main/java/rocketseat/ceccoff/com/passin/repositories/CheckInRepository.java
@@ -3,5 +3,10 @@ package rocketseat.ceccoff.com.passin.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import rocketseat.ceccoff.com.passin.domain.checkin.Checkin;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface CheckInRepository extends JpaRepository<Checkin, Integer> {
+
+    Optional<Checkin> findByAttendeeId(String attendeeId);
 }
