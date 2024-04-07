c1b97005881c57189218fea076733b142e691ed4
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-04-07 18:05:17 -0300

    feat: :sparkles: Transformado em Record

--- src/main/java/rocketseat/ceccoff/com/passin/dto/attendee/AttendeesListResponseDTO.java
+++ src/main/java/rocketseat/ceccoff/com/passin/dto/attendee/AttendeesListResponseDTO.java
@@ -1,10 +1,6 @@
 package rocketseat.ceccoff.com.passin.dto.attendee;
 
-import lombok.Getter;
-
 import java.util.List;
 
-@Getter
-public class AttendeesListResponseDTO {
-    List<AttendeeDetails> attendees;
+public record AttendeesListResponseDTO (List<AttendeeDetails> attendees) {
 }
