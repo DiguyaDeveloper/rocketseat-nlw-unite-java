85de78bec82c1a819a25ad862cc4f9174325adc8
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-04-07 17:32:48 -0300

    feat: :sparkles: Adicionado DTO para listar Attendees

---
+++ src/main/java/rocketseat/ceccoff/com/passin/dto/attendee/AttendeesListResponseDTO.java
@@ -0,0 +1,10 @@
+package rocketseat.ceccoff.com.passin.dto.attendee;
+
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+public class AttendeesListResponseDTO {
+    List<AttendeeDetails> attendees;
+}
