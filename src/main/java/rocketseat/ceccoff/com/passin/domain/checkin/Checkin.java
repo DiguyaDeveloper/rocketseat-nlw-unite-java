4d1a63f803cca0cf21b99c9c48c70b8b608588fd
DiguyaDeveloper <diegocecconcwb@outlook.com>
2024-04-07 16:03:26 -0300

    feat: :sparkles: Adicionado classes de dominio com lombok e jakarta persistence para mapeamento da base com dominio

---
+++ src/main/java/rocketseat/ceccoff/com/passin/domain/checkin/Checkin.java
@@ -0,0 +1,31 @@
+package rocketseat.ceccoff.com.passin.domain.checkin;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import rocketseat.ceccoff.com.passin.domain.attendee.Attendee;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "check_ins")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Checkin {
+
+    @Id
+    @Column(nullable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "created_at")
+    private LocalDateTime createdAt;
+
+    @OneToOne
+    @JoinColumn(name = "attendee_id", nullable = false)
+    private Attendee attendee;
+}
